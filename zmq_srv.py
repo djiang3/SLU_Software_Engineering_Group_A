8c5f81afb53121b9a169a8d60262a7ad9b7cb211
Darrel Jiang <djiang31@yahoo.com>
2013-02-07 20:59:05 -0600

    Added push_tweet support into the zmq server program.

--- zmq_srv.py
+++ zmq_srv.py
@@ -1,7 +1,7 @@
 #
-#   Hello World server in Python
+#   A server to handle pushes and pulls of stock and tweet information.
 #   Binds REP socket to tcp://*:5555
-#   Expects "Hello" from client, replies with "World"
+#   Expects a type from the client and will send a confirmation reply accordingly.
 #
 import zmq
 import time
@@ -10,24 +10,23 @@ import getyql
 import json
 import pprint
 
-#connect to zmq
+# Connect to the zmq server.
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
  
-#connet to database
+# Connect to the database.
 sdb = getyql.simpledb()
 c = sdb.conn.cursor()
 
+# Have the server run forever.
 while True:
-    #  Wait for next request from client
+	
+    # Wait for the next request from the client and load the message.
     message = socket.recv()
     rcvd = json.loads(message)
 
-    #  Do some 'work'
-    time.sleep (1)        #   Do some 'work'
-
-    # interpret the query and run database operation
+    # Interpret the query based on its type value. The run the database operation accordingly.
     if rcvd['type'] == "stock_push":
       # did we receive a raw stock value to store?
       print "stock push request received for %s from %s" % (rcvd['symbol'], rcvd['clientname'])
@@ -35,6 +34,8 @@ while True:
       sdb.conn.commit()
       print "added %s into database" % rcvd['symbol']
       socket.send("Ack")
+
+	# Handler for stock_pull type.
     elif rcvd['type'] == "stock_pull":
       print "stock pull request received for %s from %s" % (rcvd['symbol'], rcvd['clientname'])
       pulled_stocks = []
@@ -42,7 +43,19 @@ while True:
         pulled_stocks.append(row)
       message = json.dumps(pulled_stocks)
       socket.send(message)
-    else:
-      #  Send reply back to client
+
+    # Handler for tweet_push type.
+    elif rcvd['type'] == "tweet_push":
+        print "tweet recieved with a sentiment of %s" % (rcvd['sentiment'])
+        c.execute("INSERT INTO tweets VALUES(NULL, '%s')" % (rcvd['sentiment']))
+        sdb.conn.commit()
+        print "added %s into database" % rcvd['sentiment']
+        socket.send("Ack")
+  
+  	else:
+      # Send reply back to client that the query is unspecified.
       print "received unknown query, ignoring"
       socket.send("Ack")
+
+
+
