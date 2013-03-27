77a023ff11227006c6d34692a72057c9b7a374f5
Devon Mahn <dmahn1@slu.edu>
2013-03-26 20:21:06 -0500

    New Tweet Trender
    Now returns num of negative, positive, and neutral tweets to DB.

--- getyql.py
+++ getyql.py
@@ -34,10 +34,11 @@ class simpledb:
     c.execute('''CREATE TABLE tweets (tweetsID INTEGER PRIMARY KEY, timestamp TEXT, company TEXT, sentiment TEXT, id TEXT, tweet TEXT)''')
   
     c.execute('''CREATE TABLE trendPoints
-                  (trendID INTEGER PRIMARY KEY, dateRange TEXT, company TEXT, dataType TEXT, dataValue INTEGER, dataVolume INTEGER)''')
+                  (trendID INTEGER PRIMARY KEY, dateRange TEXT, company TEXT, averageValue INTEGER, positive INTEGER, negative INTEGER, nuetral INTEGER, dataVolume INTEGER)''')
 
     c.execute('''CREATE TABLE trendMap
                   (trendID INTEGER, tweetID INTEGER)''')
+    
     c.execute('''CREATE TABLE subTrendMap
                   (trendID INTEGER, subTrendID INTEGER)''')
     self.conn.commit()
